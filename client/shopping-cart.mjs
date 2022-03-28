8cd64664ebbe72da5073fccadbc0980c32ba08f9
UP2028963 <87422366+UP2028963@users.noreply.github.com>
2022-03-28 08:14:51 +0100

    quantity now working

--- client/shopping-cart.mjs
+++ client/shopping-cart.mjs
@@ -6,13 +6,28 @@ export const dummyIDs = [];
 export async function arrayOfBricks(e) {
   const brickID = e.target.parentElement.firstChild.dataset.id;
   const response = await fetch('/bricks/' + brickID);
-  if (response.ok && e.target.nextSibling.value > 0 && dummyIDs.indexOf(brickID) == -1) {
+  if (response.ok) {
     const rawDetails = await response.json();
+    rawDetails.count = Number(e.target.nextSibling.value);
 
-    cartContents.push(rawDetails);
-    dummyIDs.push(rawDetails.id);
-    addToLocalStorage();
-    console.log(cartContents);
+    if (dummyIDs.indexOf(rawDetails.id) == -1 && e.target.nextSibling.value > 0) {
+      cartContents.push(rawDetails);
+      dummyIDs.push(rawDetails.id);
+      addToLocalStorage();
+    } else if (dummyIDs.indexOf(rawDetails.id) != -1 && e.target.nextSibling.value > 0) {
+      // console.log('hello ice');
+      const storedBricks = JSON.parse(window.localStorage.getItem('basket'));
+      for (const storedBrick of storedBricks) {
+        if (storedBrick.id == rawDetails.id) {
+          // console.log(storedBrick.count);
+
+          storedBrick.count = Number(storedBrick.count) + rawDetails.count;
+          window.localStorage.setItem('basket', JSON.stringify(storedBricks));
+          console.log(storedBrick.count);
+        }
+      }
+    }
+    // console.log(cartContents);
   } else {
     console.log('failed to send message', response);
   }
@@ -21,3 +36,4 @@ export async function arrayOfBricks(e) {
 function addToLocalStorage() {
   window.localStorage.setItem('basket', JSON.stringify(cartContents));
 }
+
