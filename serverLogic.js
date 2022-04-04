f0c1c2e8268408e46d1f6cd860a42433f17f87a0
UP2028963 <87422366+UP2028963@users.noreply.github.com>
2022-04-04 18:33:38 +0100

    database connected

--- serverLogic.js
+++ serverLogic.js
@@ -1,26 +1,34 @@
 // adapted from https://github.com/portsoc/staged-simple-message-board/commit/stage-5
-import { bricks } from './data-testing.js';
+// import { bricks } from './data-testing.js';
 import sqlite from 'sqlite';
-import sqlite3 from 'sqlite3';
+// import sqlite3 from 'sqlite3';
 
 async function init() {
-  const db = await sqlite.open('./database.sqlit', { verbose: true });
+  const db = await sqlite.open('./database.sqlite', { verbose: true });
   await db.migrate({ migrationsPath: './migrations-sqlite' });
   return db;
 }
 
+// async function init() {
+// await open({
+// filename: './database.sqlite',
+// verbose: true,
+// driver: sqlite3.Database,
+// }).then((db) => {
+// db.migrate({ migrationsPath: './migrations-sqlite' });
+// return db;
+// });
+// }
+
 const dbConn = init();
 
-export function listBricks() {
-  return bricks;
+export async function listBricks() {
+  const db = await dbConn;
+  return db.all('SELECT * FROM Bricks');
+  // return db.all('DROP TABLE Bricks');
 }
 
-export function findBrick(id) {
-  for (const brick of bricks) {
-    // eslint-disable-next-line eqeqeq
-    if (brick.id == id) {
-      return brick;
-    }
-  }
-  return null;
+export async function findBrick(id) {
+  const db = await dbConn;
+  return db.get('SELECT * FROM Bricks WHERE id = ?', id);
 }
