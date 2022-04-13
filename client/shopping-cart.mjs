1526da565cdcf24a540bea83e3dc037c00e0ba64
UP2028963 <87422366+UP2028963@users.noreply.github.com>
2022-04-13 23:37:03 +0100

    quantity local storage initial attempt

--- client/shopping-cart.mjs
+++ client/shopping-cart.mjs
@@ -3,7 +3,7 @@
 // adapted from https://github.com/portsoc/staged-simple-message-board/blob/master/stages/4/client/index.js
 export const cartContents = [];
 export const dummyIDs = [];
-// const cartPriceTotal = [];
+const cartItemQuantity = [];
 
 export async function addToLocalStorage(e) {
   const brickID = e.target.parentElement.firstChild.dataset.id;
@@ -14,8 +14,18 @@ export async function addToLocalStorage(e) {
     const brickids = window.localStorage.getItem('brickids');
     // console.log(rawDetails.count);
     // rawDetails.count = Number(e.target.nextSibling.value);
+    if (brickids == null) {
+      rawDetails.count = Number(e.target.nextSibling.value);
+      cartItemQuantity.push(rawDetails.count);
+      dummyIDs.push(rawDetails.id);
+      window.localStorage.setItem('brickids', dummyIDs);
 
-    if (dummyIDs.indexOf(rawDetails.id) == -1 && e.target.nextSibling.value > 0) {
+      cartContents.push(rawDetails);
+      // possibly creating a local storage for the quantity only
+      // window.localStorage.setItem('quantity', )
+      window.localStorage.setItem('basket', JSON.stringify(cartContents));
+      // console.log('hello world');
+    } else if (brickids.indexOf(rawDetails.id) == -1 && e.target.nextSibling.value > 0) {
       rawDetails.count = Number(e.target.nextSibling.value);
       // console.log(rawDetails);
       cartContents.push(rawDetails);
@@ -23,7 +33,7 @@ export async function addToLocalStorage(e) {
       window.localStorage.setItem('brickids', dummyIDs);
 
       window.localStorage.setItem('basket', JSON.stringify(cartContents));
-    } else if (dummyIDs.indexOf(rawDetails.id) != -1 && e.target.nextSibling.value > 0) {
+    } else if (brickids.indexOf(rawDetails.id) != -1 && e.target.nextSibling.value > 0) {
       // console.log('hello ice');
       // const storedBricks = JSON.parse(window.localStorage.getItem('basket'));
       for (const storedBrick of cartContents) {
@@ -40,16 +50,36 @@ export async function addToLocalStorage(e) {
     console.log('failed to send message', response);
   }
   // addToBasket();
-  cartTally();
+  // cartTally();
 }
 
-function cartTally() {
-  const cartCounter = document.querySelector('.total-count');
-  const storedBricks = JSON.parse(window.localStorage.getItem('basket'));
+// function cartTally() {
+// const cartCounter = document.querySelector('.total-count');
+// const storedBricks = JSON.parse(window.localStorage.getItem('basket'));
+//
+// let brickTally = 0;
+// for (const storedBrick of storedBricks) {
+// brickTally += storedBrick.count;
+// cartCounter.textContent = brickTally;
+// }
+// }
+//
 
-  let brickTally = 0;
-  for (const storedBrick of storedBricks) {
-    brickTally += storedBrick.count;
-    cartCounter.textContent = brickTally;
-  }
-}
+// function cartTally() {
+// let brickTally = 0;
+// const cartCounter = document.querySelector('.total-count');
+// const storedBricks = JSON.parse(window.localStorage.getItem('basket'));
+//
+// if (storedBricks == null) {
+// console.log('hello world');
+// cartCounter.textContent = 0;
+// } else {
+// for (const storedBrick of storedBricks) {
+// brickTally += storedBrick.count;
+// cartCounter.textContent = brickTally;
+// }
+// }
+// }
+//
+// window.addEventListener('load', cartTally);
+//
